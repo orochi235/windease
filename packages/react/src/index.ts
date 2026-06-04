85412faf26e920a9a913d9dbdcaeeedd98482f9a
Michael Baker <contact@michaelbaker.tech>
2026-06-04 13:49:58 -0400

    feat(react): export Workspace

--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -1,3 +1,4 @@
 export { WindeaseProvider, WindeaseContext } from './WindeaseProvider.js';
 export { Zone } from './Zone.js';
+export { Workspace } from './Workspace.js';
 export { useWindease, useWindow, useZone, useWindowsByZone } from './hooks.js';
